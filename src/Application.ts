dae90727a7070b34f121423c68c39d486ffdb013
mohdshanil <mohdshanil@yahoo.co.in>
2017-05-05 09:10:19 +0530

    Added Spirite frame configurations

--- src/Application.ts
+++ src/Application.ts
@@ -60,6 +60,39 @@ export class Application {
         this._backgroundLayer.addChild(gfx);
     }
 
+protected createSpriteFrames():void{
+        // create an array of textures from an image path
+    let frames = [];
+
+    for (let i = 0; i < 30; i++) {debugger;
+        let val = i < 10 ? '0' + i : i;
+
+        // magically works since the spritesheet was loaded with the pixi loader
+        frames.push(PIXI.Texture.fromFrame('rollSequence00' + val + '.png'));
+    }
+    
+    // create an AnimatedSprite (brings back memories from the days of Flash, right ?)
+    let anim = new PIXI.extras.AnimatedSprite(frames);
+
+    /*
+     * An AnimatedSprite inherits all the properties of a PIXI sprite
+     * so you can change its position, its anchor, mask it, etc
+     */
+    anim.x = app.renderer.width / 2;
+    anim.y = app.renderer.height / 2;
+    anim.anchor.set(0.5);
+    anim.animationSpeed = 0.5;
+    anim.play();
+
+     this._foregroundLayer.addChild(anim);
+
+    // Animate the rotation
+    app.ticker.add(function() {
+        anim.rotation += 0.01;
+    });
+       
+    }
+
     public getTexture(name:string):PIXI.Texture{
         return this._resources[name].texture;
     }
