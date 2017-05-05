937ba6660d6d1173d9c26c44ef146ff57254acc4
mohdshanil <mohdshanil@yahoo.co.in>
2017-05-05 16:40:42 +0530

    Minum Req completed

--- src/BigWin.ts
+++ src/BigWin.ts
@@ -6,23 +6,39 @@ import {Application, IAssetsDictionary} from "./Application";
  * @author jowa
  */
 export class BigWin extends Application {
-private i:number;
-private textures:any;
-private requestAnimationFrame:any;
+
+  protected bidStateText: PIXI.Text;
+  protected winStateText: PIXI.Text;
+  protected initailWinAmount: number;
+  protected finalWinAmount: number;
+  protected betAmount: number;
+  protected betRatio: number;
+  protected counter: number;
+
     constructor() {
         let assets: IAssetsDictionary = {
             "bw-intro": "animations/bw-intro.json"
         };
+          super(assets, "../media/");
 
-        super(assets, "../media/");
+        // this.bidStateText.text ="";
+        //   this.winStateText.text ="";
+
 
     }
 
 
-    protected initAnimations(): void {
-        
-        super.initAnimations();
-        
+
+    protected initAnimations(): void {debugger;
+
+      //  super.initAnimations();
+
+      this.initailWinAmount= 0;
+      this.finalWinAmount = 1000;
+      this.betAmount =10;
+      this.betRatio= 0;;
+      this.counter =0;
+
         let textStyle: PIXI.TextStyleOptions = {
             "fontFamily": "Futura LT",
             "fontSize": "40px",
@@ -30,21 +46,47 @@ private requestAnimationFrame:any;
             "fill": "#00FF00"
         };
 
-        let winStateText: PIXI.Text = new PIXI.Text("BIG WIN", textStyle);
-        winStateText.anchor.set(0.5, 0.5);
-        winStateText.style.fill = 0xEAB348;
-        winStateText.position.set(this._stageWidth * 0.5, this._stageHeight * 0.5 - 160);
-        this._foregroundLayer.addChild(winStateText);
+        this.winStateText = new PIXI.Text("", textStyle);
+        this.winStateText.anchor.set(0.5, 0.5);
+        this.winStateText.style.fill = 0xEAB348;
+        this.winStateText.position.set(this._stageWidth * 0.5, this._stageHeight * 0.5 - 120);
+        this._foregroundLayer.addChild(this.winStateText);
 
-        let bidStateText: PIXI.Text = new PIXI.Text("0", textStyle);
-        bidStateText.anchor.set(0.5, 0.5);
-        bidStateText.style.fill = 0xEAB348;
-        bidStateText.position.set(this._stageWidth * 0.5, this._stageHeight * 0.6);
-        this._foregroundLayer.addChild(bidStateText);
-        
+        this.bidStateText = new PIXI.Text("0", textStyle);
+        this.bidStateText.anchor.set(0.5, 0.5);
+        this.bidStateText.style.fill = 0xEAB348;
+        this.bidStateText.position.set(this._stageWidth * 0.5, this._stageHeight * 2.6);
+        this._foregroundLayer.addChild(this.bidStateText);
+            this.incrementCount();
 
 
     }
 
-   
+
+    public incrementCount(this:any):void{
+
+      if(this.counter === 1000){
+        // TODO
+      }
+        if((this.counter/ this.betAmount) < 15){
+          this.winStateText.text = "";
+        } else if((this.counter/ this.betAmount) >= 15 && (this.counter/ this.betAmount) <= 30){
+            this.winStateText.text ="BIG WIN";
+        }
+        else if((this.counter/ this.betAmount) > 30 && (this.counter/ this.betAmount) <= 60){
+            this.winStateText.text ="MEGA WIN";
+        }
+        else {
+          this.winStateText.text ="SUPER MEGA WIN";
+        }
+        this.counter++;
+        this.bidStateText.text = this.counter.toString();
+      requestAnimationFrame(()=>this.incrementCount());
+
+
+
+    }
+
+
+
 }
